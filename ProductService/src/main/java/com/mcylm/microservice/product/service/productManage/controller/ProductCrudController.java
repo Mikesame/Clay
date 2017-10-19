ee921c4b71f43720f6fd971008625b1283ac27d7
Mikesam <1627072456@qq.com>
2017-10-19 11:25:09 +0800

    初始化产品服务

---
+++ ProductService/src/main/java/com/mcylm/microservice/product/service/productManage/controller/ProductCrudController.java
@@ -0,0 +1,4 @@
+package com.mcylm.microservice.product.service.productManage.controller;
+
+public class ProductCrudController {
+}
---
+++ ProductService/src/main/java/com/mcylm/microservice/product/service/productManage/service/impl/ProductCrudServiceImpl.java
@@ -0,0 +1,9 @@
+package com.mcylm.microservice.product.service.productManage.service.impl;
+
+import com.mcylm.microservice.product.service.productManage.service.ProductCrudService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductCrudServiceImpl implements ProductCrudService {
+
+}
