ccf8721b2ec977bfa92e284678ad1e65a5602f59
Mikesam <1627072456@qq.com>
2017-10-20 14:43:20 +0800

    将配置信息转移到bootstrap.properties内，清空application.properties

---
+++ OAuthService/src/main/java/com/mcylm/microservice/oauth/service/oauth2Implements/controller/OAuthController.java
@@ -0,0 +1,13 @@
+package com.mcylm.microservice.oauth.service.oauth2Implements.controller;
+
+import com.mcylm.microservice.oauth.service.utils.Result;
+import com.mcylm.microservice.oauth.service.utils.ResultType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuthController {
+
+
+}
