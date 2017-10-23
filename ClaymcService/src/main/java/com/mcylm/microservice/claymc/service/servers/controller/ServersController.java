4bb793149b266a9a7a08af9d654e9a1080e687b9
Mikesam <1627072456@qq.com>
2017-10-23 20:56:45 +0800

    修改了config server的配置文件

--- ClaymcService/src/main/java/com/mcylm/microservice/claymc/service/servers/controller/ServersController.java
+++ ClaymcService/src/main/java/com/mcylm/microservice/claymc/service/servers/controller/ServersController.java
@@ -3,6 +3,7 @@ package com.mcylm.microservice.claymc.service.servers.controller;
 import com.mcylm.microservice.claymc.service.model.po.Clay_mc_servers_po;
 import com.mcylm.microservice.claymc.service.servers.service.ServersService;
 import com.mcylm.microservice.claymc.service.utils.Result;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Administrator on 2017/10/21.
  */
 @RestController
+@Api(value = "【Mikesam】服务器部分数据操作", tags = { "【Mikesam】服务器部分数据操作" })
 @RequestMapping("/server")
 public class ServersController {
 
@@ -35,11 +37,11 @@ public class ServersController {
         return serversService.updateMineCraftServerByParamsWithOutLogin(clay_mc_serversPo);
     }
 
-    @ApiOperation(value="【查询】查询一个MC服务器信息，无需登录", notes="测试接口详细描述")
-    @ApiImplicitParam(name = "clay_mc_serversPo", value = "服务器详细实体clay_mc_servers", required = true, dataType = "Clay_mc_servers_po")
-    @RequestMapping(value = "/selectMineCraftServers",method = {RequestMethod.GET,RequestMethod.POST})
-    public Result selectMineCraftServers(@RequestBody Clay_mc_servers_po clay_mc_serversPo){
-        return serversService.selectMineCraftServerByParams(clay_mc_serversPo);
+    @ApiOperation(value="【查询】查询一个MC服务器信息，无需登录", notes="实例参数模板：{ \"server_host\": \"127.0.0.1\", \"server_no\": 12, \"server_title\": \"乐游聚乐\" },默认如果用户不填写任何查询条件的话，就用空字符串传给后台！")
+    @ApiImplicitParam(name = "clay_mc_servers_po", value = "服务器详细实体clay_mc_servers_po", required = true, dataType = "Clay_mc_servers_po")
+    @RequestMapping(value = "/selectMineCraftServers",method = {RequestMethod.POST})
+    public Result selectMineCraftServers(@RequestBody Clay_mc_servers_po clay_mc_servers_po){
+        return serversService.selectMineCraftServerByParams(clay_mc_servers_po);
     }
 
 }
